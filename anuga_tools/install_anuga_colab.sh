fded29b30710f90a08d3ce468303a3d7728c10e8
wrightky <kyleawright@utexas.edu>
2022-03-25 13:57:12 -0500

    add build step

--- anuga_tools/install_anuga_colab.sh
+++ anuga_tools/install_anuga_colab.sh
@@ -2,13 +2,13 @@
 
 cd /content
 
-echo "(1) Install nose via pip"
+echo "(1) Install pip packages"
 pip -q install nose mpi4py triangle Pmw pymetis  > /dev/null 2>&1 
 
-echo "(2) Install gdal via apt-get"
+echo "(2) Install gdal"
 apt-get -q -y install python-gdal gdal-bin  > /dev/null 2>&1 
 
-echo "(3) Install netcdf4 via apt-get"
+echo "(3) Install netcdf4"
 apt-get -q -y install python-netcdf4  > /dev/null 2>&1 
 
 echo "(4) Download anuga_core github repository"
@@ -17,6 +17,7 @@ git clone --quiet https://github.com/GeoscienceAustralia/anuga_core.git  > /dev/
 echo "(5) Install anuga"
 
 cd anuga_core
+python setup.py --quiet build  > /dev/null 2>&1 
 python setup.py --quiet install  > /dev/null 2>&1 
 cd ../
 
